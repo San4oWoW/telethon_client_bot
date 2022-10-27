2b68ca5630ac7ea716c799d09802502e7aa65e6f
Аликин Александр Сергеевич <alial@cupola.local>
2022-10-27 18:00:01 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from telethon import TelegramClient, client
+from telethon.tl.types import SendMessageUploadDocumentAction
+from telethon.utils import pack_bot_file_id
+import datetime
+api_id = 28968521
+api_hash = "f15b15aecaefaac2fc9fe4b0a0469502"
+name = "alexwow"
+
+client = TelegramClient(name, api_id, api_hash)
+
+async def main():
+    # Now you can use all client methods listed below, like for example...
+    start = datetime.datetime.now()
+    r = await client.send_file('me', r'W:\10_ИТ отдел\Техподдержка\1Cv8.dt')
+    stop = datetime.datetime.now()
+
+    finish = stop - start
+    print(finish)
+    # file_id = pack_bot_file_id(r.media)
+    # print(file_id)
+    # print("Успешно")
+    # await client.send_file("me", "BQADAgAD5CEAAsovwEpXzVPKQcAVLwI")
+
+with client:
+    client.loop.run_until_complete(main())
